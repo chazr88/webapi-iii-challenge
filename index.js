0cbcbaa44613806835083036c4955c7e1b5e3843
chazr88 <chazr_88@hotmail.com>
2019-08-02 11:22:01 -0400

    updated index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const server = require('./server.js');
 require('dotenv').config();
 
-const port = process.env.PORT || 5000;
-
-server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
